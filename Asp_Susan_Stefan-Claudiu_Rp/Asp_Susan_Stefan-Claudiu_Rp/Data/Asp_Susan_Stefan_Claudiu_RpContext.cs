b3196eeafa7848cb838ee2e36796d743aca434d3
CDU55 <48677296+CDU55@users.noreply.github.com>
2020-05-06 16:43:01 +0300

    Laborator Razor Pages

---
+++ Asp_Susan_Stefan-Claudiu_Rp/Asp_Susan_Stefan-Claudiu_Rp/Data/Asp_Susan_Stefan_Claudiu_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Susan_Stefan_Claudiu_Rp.Models;
+
+namespace Asp_Susan_Stefan_Claudiu_Rp.Data
+{
+    public class Asp_Susan_Stefan_Claudiu_RpContext : DbContext
+    {
+        public Asp_Susan_Stefan_Claudiu_RpContext (DbContextOptions<Asp_Susan_Stefan_Claudiu_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Susan_Stefan_Claudiu_Rp.Models.Movie> Movie { get; set; }
+    }
+}
